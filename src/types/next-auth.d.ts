a241e79c0d0498a2a2087c20902a4147171b78f4
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2024-01-19 18:50:36 +0600

    Login, logout and verification done

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import { UserRole } from "@prisma/client";
+import type { User } from "next-auth";
+import "next-auth";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: UserRole;
+    // isOuth: boolean;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      id: string;
+      role?: UserRole;
+      isOuth: boolean;
+    };
+  }
+}
